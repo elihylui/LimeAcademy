8f9f932341969e906fcb8c5665e0f275e36caacb
Elizabeth <elihylui@gmail.com>
2022-10-20 17:53:11 +0100

    add config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,12 @@ const config: HardhatUserConfig = {
         enabled: true,
         runs: 200,
       },
+    },
+  },
+  networks: {
+    rinkeby: {
+      url: "https://goerli.infura.io/v3/6b36b2e9d5e14cc4af78d5e905cc38f7",
+      accounts: ['42575f4941c226ecd6180a70e8aaa7063ca935253a4966640b4adf308e4ba8bd']
     }
   }
 };
