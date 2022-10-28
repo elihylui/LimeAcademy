eb0e5000513102fb5919901f3246d63a58aeb4d8
Elizabeth <elihylui@gmail.com>
2022-10-28 22:00:08 +0100

    fix deployment to local & goerli

---
+++ new/hardhat.config.ts
@@ -0,0 +1,39 @@
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [{  version: "0.8.17",}],
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    goerli: {
+      url: "https://goerli.infura.io/v3/6b36b2e9d5e14cc4af78d5e905cc38f7",
+      accounts: ['42575f4941c226ecd6180a70e8aaa7063ca935253a4966640b4adf308e4ba8bd']
+    }
+  },
+  etherscan: {
+    apiKey: "CHIRAADNUI814XIT9ST36R63UFNBNDKBDY"
+  }
+};
+
+task("deploy-testnets", "Deploys contract on a provided network")
+    .setAction(async () => {
+        const main = require("./scripts/deploy");
+        await main();
+    });
+
+// task("deploy-mainnet", "Deploys contract on a provided network")
+// .addParam("privateKey", "Please provide the private key")
+// .setAction(async ({privateKey}) => {
+//     const deployElectionContract = require("./scripts/deploy-with-param");
+//     await deployElectionContract(privateKey);
+// });
+
+export default config;
---
+++ new/scripts/deploy.ts
@@ -0,0 +1,22 @@
+import hre from 'hardhat';
+const ethers = hre.ethers;
+
+async function main() {
+  await hre.run('compile'); // We are compiling the contracts using subtask
+  const [deployer] = await ethers.getSigners(); // We are getting the deployer
+
+  console.log('Deploying contracts with the account:', deployer.address); // We are printing the address of the deployer
+  console.log('Account balance:', (await deployer.getBalance()).toString()); // We are printing the account balance
+
+  const USElection = await hre.ethers.getContractFactory("USElection");
+  const usElection = await USElection.deploy();
+  await usElection.deployed();
+  console.log("USElection deployed to:", usElection.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
+module.exports = main;
